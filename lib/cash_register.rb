1e466fdb461401d5c844cc21ff7fe20c8870a526
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-01-29 18:32:42 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,25 @@
+class CashRegister
 
+  attr_accessor :total, :discount, :quantity, :title, :price
+
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+  end
+
+  def total
+    @total
+  end
+
+  def add_item(title, price, quantity = 1)
+    @total = price * quantity
+  end
+
+  def apply_discount
+    discount_percent = 100 - @discount
+    new_price = 100/discount_percent
+    new_total = total * new_price
+    puts "After the discount, the total comes to $#{new_total}"
+  end
+
+end
